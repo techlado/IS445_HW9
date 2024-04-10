1b74822639b3fd268857166a2173432ef7291700
Danielle Gonzalez <15gonzalezdanielle@gmail.com>
2024-04-10 15:36:29 -0700

    Ex2 update

--- ex2/accounting.js
+++ ex2/accounting.js
@@ -1,6 +1,12 @@
 class Account {
-  constructor(owner) {
-    this.credit;
-    this.describe;
+  constructor(owner, accountTotal = 0.00) {
+    this.owner = owner;
+    this.accountTotal = accountTotal;
+  }
+  credit = (amount) => {
+    this.accountTotal += amount;
+  }
+  describe = () => {
+    return this.owner, `Balance: ${this.accountTotal}`;
   }
 }
